d1959ebd4e79a0be0eef5026de3bc2f63f5c2f4e
Dong Du <dd_nirvana@sjtu.edu.cn>
2022-06-17 22:58:34 +0800

    script: update run script to use raw image file (with hpmp benchsuites)
    Signed-off-by: Dong Du <dd_nirvana@sjtu.edu.cn>

--- run_openeuler.sh
+++ run_openeuler.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 qemu-system-riscv64 -nographic -machine virt \
-	-smp 1 -m 2G \
+	-smp 1 -m 8G \
 	-kernel  ./opensbi-0.9/build-oe/qemu-virt/platform/generic/firmware/fw_payload.elf  \
-	-drive file=openEuler-preview.riscv64.qcow2,format=qcow2,id=hd0 \
+	-drive file=openeuler.riscv64.raw,format=raw,id=hd0 \
 	-object rng-random,filename=/dev/urandom,id=rng0 \
 	-device virtio-rng-device,rng=rng0 \
 	-device virtio-blk-device,drive=hd0  \
@@ -11,6 +11,7 @@ qemu-system-riscv64 -nographic -machine virt \
 	-append 'root=/dev/vda1 rw console=ttyS0 systemd.default_timeout_start_sec=600 selinux=0 highres=off mem=4096M earlycon' \
 	-bios none
 
+	#-drive file=openEuler-preview.riscv64.qcow2,format=raw,id=hd0 \
 
 ## For v0.9, use the following path
 	#-kernel  ./opensbi-0.9/build-oe/qemu-virt/platform/generic/firmware/fw_payload.elf  \
